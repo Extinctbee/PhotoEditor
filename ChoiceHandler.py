aba8120109c6071ba5f410fb3164a0716558f19b
Extinctbee <Extinctbee321@gmail.com>
2026-01-08 08:41:40 -0600

    commented on all the code I needed to.

--- ChoiceHandler.py
+++ ChoiceHandler.py
@@ -2,7 +2,7 @@ from PIL import ImageFilter , ImageEnhance , Image
 
 
 def editChoice(img,history):
-    edits = {
+    edits = { # creates dictionary that maps users input to actual edit features
         "sharpen" : ImageFilter.SHARPEN ,
         "blur"    : ImageFilter.BLUR ,
         "contour" : ImageFilter.CONTOUR ,
@@ -10,12 +10,13 @@ def editChoice(img,history):
         "smooth"  : ImageFilter.SMOOTH
 
     }
-    while True:
+    while True: # keeps running prompt for user choice
 
 
         user_choice = input("Enter an edit (sharpen , blur , contour , detail , smooth) or undo :").lower().strip()
 
-        if user_choice == 'undo' :
+        if user_choice == 'undo' : #if the user wants to undo and an edit it will be "popped" out of the stack
+            # and bring the image back to the previous version
             if len(history) > 0 :
                 history.pop()
                 img.show()
@@ -23,13 +24,16 @@ def editChoice(img,history):
             else :
                 print("nothing to undo")
         elif user_choice == 'save':
+            #This input is what ultimately allows the user move on to the next image they want to edit
+            #until they are done with all the images
             return "SAVE_AND_EXIT"
-        selected_filter = edits.get(user_choice)
+        selected_filter = edits.get(user_choice) #gets the user choice and fetches the edit type
         if selected_filter:
-            history.append(img.copy())
-            img = img.filter(selected_filter)
+            history.append(img.copy()) #adds the current version of the image before the edit and will
+            #continue adding the most recent version
+            img = img.filter(selected_filter) # this changes the image to the edited version
             print(f"Successfully applied {user_choice}")
-            img.show()
+            img.show() #shows the edited image
         else:
             print("Invalid edit name please try again")
         return img
--- main.py
+++ main.py
@@ -4,17 +4,20 @@ from ChoiceHandler import editChoice
 path = './imgs' # folder for unedited images
 pathOut = '/editedImgs' # folder for edited images
 
-for filename in os.listdir(path):
+for filename in os.listdir(path): # goes through every file in the images folder and opens it
     img = Image.open(f"{path}/{filename}")
-    current_img = img
-    history = []
-    while True:
-        edit = editChoice(img,history)
-        if edit == "SAVE_AND_EXIT":
+    current_img = img # puts the image into current image so that it can be tracked
+
+    history = [] # creates list we will use to store versions of the image
+
+    while True: # keeps prompting edits until the user has saved all the edits and is done
+        edit = editChoice(img,history) #calls the edit choice and passes the image and it's history
+
+        if edit == "SAVE_AND_EXIT": #stops loop if user chooses to save
             break
         current_img = edit
     clean_name = os.path.splitext(filename)[0]
 
-    current_img.save(f'.{pathOut}/{clean_name}_edited.jpg')
+    current_img.save(f'.{pathOut}/{clean_name}_edited.jpg')  #saves the image to the edited images folder
     print(f"Final Version of {clean_name} saved!")
 
